b621d6527d91f64b07d2ac9813af578a2489cce0
Junior Moreira Martins <junior.martins@neogrid.com>
2021-11-08 13:22:01 -0300

    Completed data structure stack sample.

---
+++ data-structure/src/ds/stack/StackTest.java
@@ -0,0 +1,45 @@
+package ds.stack;
+
+import java.util.Stack;
+
+public class StackTest {
+	
+	 public static void main(String[] args) {
+	       Stacks stack = new Stacks();
+	       
+	       System.out.println("stackEmpty:" + stack.empty());
+	       
+	       stack.push("Mauricio");
+	       System.out.println("push('Mauricio'):" + stack);
+
+	       stack.push("Guilherme");
+	       System.out.println("push('Guilherme'):" + stack);
+	       
+	       System.out.println("stackEmpty:" + stack.empty());
+	       	       
+	       String r1 = stack.pop();
+	       System.out.println("pop:" + r1);
+
+	       String r2 = stack.pop();
+	       System.out.println("pop:" + r2);
+
+	       System.out.println("stack:" + stack);
+	       
+	       System.out.println("stackEmpty:" + stack.empty());
+	       
+	       //Implementação java para pilhas.
+	       Stack<String> javaStack = new Stack<String>();
+	       javaStack.push("Mauricio");
+	       javaStack.push("Marcelo");
+
+	       System.out.println("javaStack:" + stack);
+	       
+	       javaStack.pop();
+	       System.out.println("javaPop:" + stack);
+	       
+	       String name = javaStack.peek();
+	       System.out.println("javaPeek:" + name);
+	       
+	       
+	    }
+}
---
+++ data-structure/src/ds/stack/Stacks.java
@@ -0,0 +1,27 @@
+package ds.stack;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Stacks {
+	
+	private List<String> names = new LinkedList<String>();
+
+	public void push(String name) {
+		names.add(name);
+	}
+	
+	public String pop() {
+		return names.remove(names.size()-1);
+	}
+	
+	public boolean empty() {
+		return names.isEmpty();
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return names.toString();
+	}
+}
